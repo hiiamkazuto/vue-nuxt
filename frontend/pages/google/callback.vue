643eb71b675a48edb3bfe7477c249704a2397d9c
MIURA, Kazuto <hiiamkazuto@gmail.com>
2024-05-16 01:49:44 +0900

    googleがよべる

--- frontend/pages/google/callback.vue
+++ frontend/pages/google/callback.vue
@@ -4,13 +4,10 @@ const params = { method: "GET", body: route.query }
 console.log(route.query);
 const query_params = new URLSearchParams(route.query)
 const loginWithGoogle = async () => {
-    // const response = await $fetch("http://localhost:8080/api/login/google/callback", params)
     const response = await $fetch(`http://localhost:8080/api/login/google/callback?${query_params}`)
     await console.log(response)
 }
 loginWithGoogle()
-
-
 </script>
 
 <template>
